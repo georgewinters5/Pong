4edd21dfdb0a0f8718c64bf13a9a78a6b30642cf
ice.Fallacy <william.paroff@student.allenisd.org>
2020-12-01 13:11:01 -0600

    changed paddle

--- Sources/ScenesShell/Paddle.swift
+++ Sources/ScenesShell/Paddle.swift
@@ -3,41 +3,25 @@ import Scenes
 
 class Paddle : RenderableEntity, KeyDownHandler, KeyUpHandler
 {
-    let rect : Rect
-    let controls : [String]
-    let rate : Int
-
-    var isMovingUp : Bool = false
-    var isMovingDown : Bool = false
-
-    init(_ rect:Rect, _ controlKeys:[String], _ rate:Int)
+    var rectangle : Rectangle
+    
+    init(topLeft:Point, size:Size)
     {
-        precondition(condition:controlKeys.count == 2)
-        rect = rect
-        controls = controlKey
-        rate = rate
-        
-        super.init(name:"Paddle")
+        let rect = Rect(topLeft:topLeft, size:size)
+        rectangle = Rectangle(rect:rect, fillMode:.fillAndStroke)
+    }
+    
+    func paint(canvas:Canvas)
+    {
+        let strokeStyle = StrokeStyle(color:Color(.black))
+        let fillStyle = FillStyle(color:Color(.white))
+        let lineWidth = LineWidth(width:2)
+        canvas.paint(strokeStyle, fillStyle, lineWidth)
+        canvas.paint(rectangle)
     }
 
-    override func render(canvas:Canvas)
+    func move(to:Point)
     {
-        let fillStyle : FillStyle = FillStyle(color:Color(.white))
-        let strokeStyle : StrokeStyle = StrokeStyle(color:Color(.black))
-        
-        let rectangle = Rectangle(rect:rect, fillMode:.fillAndStroke)
-
-        canvas.render(fillStyle, strokeStyle, rectangle)
-    }
-
-    override func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool)
-    {
-        isMovingUp = key == controls[0] ? true : isMovingUp
-        isMovingDown = key == controls[1] ? true : isMovingDown
-    }
-    override func onKeyUp(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool)
-    {
-        isMovingUp = key == controls[0] ? true : isMovingUp
-        isMovingDown = key == controls[1] ? true : isMovingDown
+        rectangle.rect.topLeft = to
     }
 }
