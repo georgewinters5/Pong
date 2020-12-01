0b179b38851b8be8e0ccf1f3807e75ae804d1456
Camden Thomson <camden.thomson@student.allenisd.org>
2020-12-01 12:21:37 -0600

    Initial commit

---
+++ .dir-locals.el
@@ -0,0 +1,11 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((swift-mode . 
+  ((lsp-sourcekit-extra-args . (
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Igis-1.3.6/Igis/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Igis-1.3.6/Igis/.build/debug" 
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/Scenes-1.1.3/Scenes/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/Scenes-1.1.3/Scenes/.build/debug" 
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/ScenesControls-0.0.9/ScenesControls/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/ScenesControls-0.0.9/ScenesControls/.build/debug" 
+	 "-Xlinker" "-L" "-Xlinker" "/usr/local/lib/merlin/ScenesAnimations-0.1.3/ScenesAnimations/.build/debug" "-Xswiftc" "-I" "-Xswiftc" "/usr/local/lib/merlin/ScenesAnimations-0.1.3/ScenesAnimations/.build/debug" 
+
+)))))
