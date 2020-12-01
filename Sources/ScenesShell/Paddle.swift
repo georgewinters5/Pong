173f42b558da96aaa27f5d20f81b66ff98edd12d
ice.Fallacy <william.paroff@student.allenisd.org>
2020-12-01 13:22:23 -0600

    changed paddle

--- Sources/ScenesShell/Paddle.swift
+++ Sources/ScenesShell/Paddle.swift
@@ -1,7 +1,7 @@
 import Igis
 import Scenes
 
-class Paddle : RenderableEntity, KeyDownHandler, KeyUpHandler
+class Paddle : RenderableEntity, KeyDownHandler
 {
     var rectangle : Rectangle
 
@@ -9,17 +9,30 @@ class Paddle : RenderableEntity, KeyDownHandler, KeyUpHandler
     {
         let rect = Rect(topLeft:topLeft, size:size)
         rectangle = Rectangle(rect:rect, fillMode:.fillAndStroke)
-    }
 
-    func paint(canvas:Canvas)
+        super.init()
+
+        handler.registerKeyDownHandler(handler:self)
+    }
+    override func teardown()
+    {
+        handler.unregisterKeyDownHandler(handler:self)
+    }
+    
+    override func render(canvas:Canvas)
     {
         let strokeStyle = StrokeStyle(color:Color(.black))
         let fillStyle = FillStyle(color:Color(.white))
         let lineWidth = LineWidth(width:2)
-        canvas.paint(strokeStyle, fillStyle, lineWidth)
-        canvas.paint(rectangle)
+        
+        canvas.render(strokeStyle, fillStyle, lineWidth)
+        canvas.render(rectangle)
+    }   
+    override func onKeyDown(key:String, code:String, ctrlKey:Bool, shiftKey:Bool, altKey:Bool, metaKey:Bool)
+    {
+        
     }
-
+        
     func move(to:Point)
     {
         rectangle.rect.topLeft = to
