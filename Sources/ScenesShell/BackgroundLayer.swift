bd7167a6ef6f06388177a2e445525ec756456310
Lucas-Vaughan <lucas.vaughan@student.allenisd.org>
2020-12-01 13:12:12 -0600

    broken scoreboard code

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -7,12 +7,15 @@ import Scenes
 
 class BackgroundLayer : Layer {
     let background = Background()
+    let scoreboard = Scoreboard()
     
     init() {
         // Using a meaningful name can be helpful for debugging
         super.init(name:"Background")
         
+        
         // We insert our RenderableEntities in the constructor
         insert(entity:background, at:.back)
+        insert(entity:scoreboard, at:.front)
     }
 }
---
+++ Sources/ScenesShell/Scoreboard.swift
@@ -0,0 +1,41 @@
+import Scenes
+import Igis
+
+/*
+ This class is responsible for rendering the scoreboard.
+ */
+
+
+//This is broken atm
+
+class Scoreboard : RenderableEntity {
+
+    // We establish our variables first
+    var leftScore : Int = 0
+    var rightScore : Int = 0
+    var location = Point(x:100, y:100)
+    var text = Text(location:location, text:"test", fillMode:.fill)
+    
+
+    
+    init() {
+
+        // Then we give our variables their starting values
+        
+        //self.location = Point(x:100, y:100)
+        //self.text = Text(location:location, text:"test", fillMode:.fill)
+        
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Scoreboard")
+    }
+
+    override func setup(canvasSize:Size, canvas:Canvas) {
+        
+    }
+
+    override func render(canvas:Canvas) {
+        
+    }
+    
+    
+}
