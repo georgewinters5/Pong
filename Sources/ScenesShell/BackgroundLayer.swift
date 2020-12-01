abb6ab513e2cc654c6bb7ed755617fb91ebb89b5
Camden Thomson <camden.thomson@student.allenisd.org>
2020-12-01 12:30:18 -0600

    Cleaned up files

--- Sources/ScenesShell/BackgroundLayer.swift
+++ Sources/ScenesShell/BackgroundLayer.swift
@@ -1,19 +1,18 @@
 import Scenes
 
-  /*
-     This class is responsible for the background Layer.
-     Internally, it maintains the RenderableEntities for this layer.
-   */
-
+/*
+ This class is responsible for the background Layer.
+ Internally, it maintains the RenderableEntities for this layer.
+ */
 
 class BackgroundLayer : Layer {
-      let background = Background()
-
-      init() {
-          // Using a meaningful name can be helpful for debugging
-          super.init(name:"Background")
-
-          // We insert our RenderableEntities in the constructor
-          insert(entity:background, at:.back)
-      }
-  }
+    let background = Background()
+    
+    init() {
+        // Using a meaningful name can be helpful for debugging
+        super.init(name:"Background")
+        
+        // We insert our RenderableEntities in the constructor
+        insert(entity:background, at:.back)
+    }
+}
